dfa7addffc1220c794b8aee9294b80e620204a17
alkoleft <alkoleft@gmail.com>
2023-05-01 17:20:23 +0300

    Доработки оформления
    * Выделение "важных" блоков
    * Отметка устаревшего метода

--- src/main/java/ru/alkoleft/bsl/doc/render/handlebars/HandleLinks.java
+++ src/main/java/ru/alkoleft/bsl/doc/render/handlebars/HandleLinks.java
@@ -15,16 +15,22 @@ import java.util.regex.Pattern;
 public class HandleLinks implements Helper<String> {
   Pattern pattern = Pattern.compile("см\\. (([\\wА-Яа-я\\.\\d]+)\\.)*([\\wА-Яа-я\\d]+)");
 
+  Pattern warningPattern = Pattern.compile("(\\n\\s*\\n|^)(Важно[\\s\\S]+)(\\n\\s*\\n|$)");
   BslContext context;
 
   @Override
   public Object apply(String context, Options options) {
-
     var matcher = pattern.matcher(context);
+    String result;
     if (matcher.find()) {
-      return matcher.replaceAll(matchResult -> replaceTo(matcher.group(0), Links.createLink(matcher.group(2), matcher.group(3)), options));
+      result = matcher.replaceAll(matchResult -> replaceTo(matcher.group(0), Links.createLink(matcher.group(2), matcher.group(3)), options));
+    } else {
+      result = context;
     }
-    return context;
+
+    result = handleWarning(result);
+
+    return result;
   }
 
   @SneakyThrows
@@ -36,6 +42,15 @@ public class HandleLinks implements Helper<String> {
 
     var context = Context.newBuilder(methodInfo.getMethod().getReturnedValue()).build();
     return baseValue + options.apply(options.fn, context).toString();
+  }
+
+  private String handleWarning(String content) {
+    // TODO Реализовать и другие блоки https://docusaurus.io/docs/markdown-features/admonitions
+    if (content.contains("Важно")) {
+      var matcher = warningPattern.matcher(content);
+      content = matcher.replaceAll(m -> m.group(1) + ":::tip важно\n\n" + matcher.group(2) + "\n\n:::" + matcher.group(3));
+    }
+    return content;
   }
 
   private String getLink(Links.Link link) {
